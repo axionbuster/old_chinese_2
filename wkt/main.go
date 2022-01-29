ed7c8d8c533ee11191adb8ee8be564cdc3311c6e
YuJin Kim <yujinkim@mines.edu>
2022-01-29 02:55:50 -0700

    Process entire lines; reject too long words

--- wkt/main.go
+++ wkt/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 )
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gocolly/colly/v2"
 )
+
+const VERSION = "0.0.1"
 
 var co = colly.NewCollector(
 	colly.AllowURLRevisit(),
@@ -24,6 +27,11 @@ type oldChinese struct {
 	Zheng   []string `json:"zh"`
 }
 
+type textResponse struct {
+	Version string `json:"v"`
+	Result  string `json:"result"`
+}
+
 var localWordCache = make(map[string]oldChinese)
 
 func ipaTrim(s string) string {
@@ -31,17 +39,22 @@ func ipaTrim(s string) string {
 	return s[2 : len(s)-1]
 }
 
-// getWordByWord scraps the content from Wiktionary
-func getWordByWord(c *gin.Context) {
-	word := c.Param("word")
+// Two cache levels
+//	First: localWordCache
+//	Second: colly.NewCollector
+//
+// The second cache reduces the response time to some dozen milliseconds (from seconds).
+// The first cache reduces that to some dozen MICRO-seconds.
 
+// This function implements it
+func commonLookup(word string) oldChinese {
 	cacheEntry, thereIs := localWordCache[word]
 	if thereIs {
-		c.JSON(http.StatusOK, cacheEntry)
+		return cacheEntry
 	}
 
 	wordStructure := oldChinese{
-		Version: "0.0.1",
+		Version: VERSION,
 		Word:    word,
 	}
 
@@ -62,19 +75,71 @@ func getWordByWord(c *gin.Context) {
 
 	// Visit
 	err := co.Visit(fmt.Sprintf("https://en.wiktionary.org/wiki/%s", word))
-	if err != nil {
-		wordStructure.Parse = false
-		c.JSON(http.StatusNotFound, wordStructure)
-	} else {
-		wordStructure.Parse = true
-		c.JSON(http.StatusOK, wordStructure)
-	}
+	wordStructure.Parse = err == nil
 	localWordCache[word] = wordStructure
+	return wordStructure
+}
+
+// getWordByWord scraps the content from Wiktionary
+func getWordByWord(c *gin.Context) {
+	word := c.Param("word")
+
+	// In UTF-8, a CJK character could have up to 4 bytes.
+	// On the other hand, the string is very possibly NOT UTF-8.
+	// IDC, it's their fault if they choose a wack encoding.
+	if len(word) > 4 {
+		c.JSON(http.StatusNotAcceptable, oldChinese{
+			Version: VERSION,
+			Parse:   false,
+			Word:    "",
+			Baxter:  nil,
+			Zheng:   nil,
+		})
+		return
+	}
+
+	wordStructure := commonLookup(word)
+	if wordStructure.Parse {
+		c.JSON(http.StatusOK, wordStructure)
+	} else {
+		c.JSON(http.StatusNotFound, wordStructure)
+	}
+}
+
+func getText(c *gin.Context) {
+	sb := strings.Builder{}
+
+	text := c.Param("text")
+	for _, r := range text {
+		// No match = verbatim rune print
+		// 1 match = print transl.
+		// 1+ matches = print transl. + '*' [asterisk; plural marker]
+		oc := commonLookup(string(r))
+		if oc.Parse {
+			if len(oc.Zheng) == 1 {
+				sb.WriteString(oc.Zheng[0])
+				sb.WriteRune(' ')
+			} else if len(oc.Zheng) > 1 {
+				sb.WriteString(oc.Zheng[0])
+				sb.WriteString("* ")
+			} else {
+				sb.WriteRune(r)
+			}
+		} else {
+			sb.WriteRune(r)
+		}
+	}
+
+	c.JSON(http.StatusOK, textResponse{
+		Version: VERSION,
+		Result:  strings.TrimSpace(sb.String()),
+	})
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/w/:word", getWordByWord)
+	router.GET("/t/:text", getText)
 
 	err := router.Run("localhost:8080")
 	if err != nil {
