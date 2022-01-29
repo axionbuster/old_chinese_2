469b659a6597fc353334be5bf79cee0795e2383e
YuJin Kim <yujinkim@mines.edu>
2022-01-29 02:08:26 -0700

    Make Go port of Wiktionary scraper

---
+++ wkt/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly/v2"
+)
+
+var co = colly.NewCollector(
+	colly.AllowURLRevisit(),
+	colly.CacheDir("./wikt_ca"),
+)
+
+type oldChinese struct {
+	Version string   `json:"v"`
+	Parse   bool     `json:"k"`
+	Word    string   `json:"w"`
+	Baxter  []string `json:"bs"`
+	Zheng   []string `json:"zh"`
+}
+
+func ipaTrim(s string) string {
+	// /*myrealipa/ ---> myrealipa
+	return s[2 : len(s)-1]
+}
+
+// getWordByWord scraps the content from Wiktionary
+func getWordByWord(c *gin.Context) {
+	word := c.Param("word")
+	wordStructure := oldChinese{
+		Version: "0.0.1",
+		Word:    word,
+	}
+
+	co.OnHTML("div.zhpron dd", func(e *colly.HTMLElement) {
+		whichIPA := e.ChildText("small i a")
+
+		switch whichIPA {
+		case "BaxterSagart":
+			rawIPA := ipaTrim(e.ChildText("span.IPAchar"))
+			wordStructure.Baxter = append(wordStructure.Baxter, rawIPA)
+			break
+		case "Zhengzhang":
+			rawIPA := ipaTrim(e.ChildText("span.IPAchar"))
+			wordStructure.Zheng = append(wordStructure.Zheng, rawIPA)
+			break
+		}
+	})
+
+	// Visit
+	err := co.Visit(fmt.Sprintf("https://en.wiktionary.org/wiki/%s", word))
+	if err != nil {
+		wordStructure.Parse = false
+		c.JSON(http.StatusNotFound, wordStructure)
+		return
+	}
+
+	wordStructure.Parse = true
+	c.JSON(http.StatusOK, wordStructure)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/w/:word", getWordByWord)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		_, err := fmt.Fprintf(os.Stderr, "There was an error with Gin.\n")
+		if err != nil {
+			return
+		}
+		return
+	}
+}
