3262271558cdf6ebbc836eccfe6816b85b2d9fe2
YuJin Kim <yujinkim@mines.edu>
2022-01-29 13:49:31 -0700

    Restyle

--- fro/pages/index.js
+++ fro/pages/index.js
@@ -17,25 +17,31 @@ export default function Home({connection}) {
     }
 
     return (
-        <div>
+        <div className={`flex flex-col items-center justify-center my-20 mx-4`}>
             <Head>
                 <title>Old Chinese Reconstructions!</title>
             </Head>
-            <div>
-                <form onSubmit={onSubmit}>
+            <div className={`m-auto max-w-7xl flex flex-col items-center justify-keft text-xl`}>
+                <h1 className={`font-bold text-4xl my-4`}>Traditional Chinese to Ancient Pronunciation</h1>
+                <form className={`m-auto flex flex-col gap-4 my-4`} onSubmit={onSubmit}>
                     <label>
-                        Your Chinese text:
-                        <input type={"text"} value={formText} onChange={onTextInput}/>
+                        <textarea value={formText} onChange={onTextInput}
+                                  placeholder={"Your Chinese Text"}/>
                     </label>
-                    <button type={"submit"}>Submit</button>
+                    <button
+                        className={`flex flex-grow-0 flex-shrink-1 items-center justify-center w-fit m-auto px-6 py-2 border-black border-2 rounded-md hover:bg-gray-200 transition-all`}
+                        type={"submit"}>
+                        <span className={`border-b-2 border-black`}>Submit</span>
+                    </button>
                 </form>
                 <p>
-                    Connection:
+                    IPA (Zhengzhang; Wiktionary):
+                    <br/>
                     {
                         typeof connection === "undefined" ||
                         !connection.hasOwnProperty("result") ||
                         connection.result === ""
-                        ? "Not provided." : connection.result
+                            ? "Not provided." : connection.result
                     }
                 </p>
             </div>
@@ -46,11 +52,20 @@ export default function Home({connection}) {
 export async function getServerSideProps(context) {
     const t = context.query.hasOwnProperty('t') ? context.query.t : "";
     console.log(`Query was ${t}`)
-    const res = await fetch(`http://wkt:8080/t/${t}`)
-    const js = await res.json().catch(() => {})
-    return {
-        props: {
-            connection: js
+    try {
+        const res = await fetch(`http://wkt:8080/t/${t}`)
+        const js = await res.json().catch(() => {
+        })
+        return {
+            props: {
+                connection: js
+            }
+        }
+    } catch (e) {
+        return {
+            props: {
+                connection: {}
+            }
         }
     }
 }
